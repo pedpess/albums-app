ac5c997705dd3ec13d32f5561408929ffadde4d4
Pedro Pessoa <pessoa.pedro91@gmail.com>
2017-11-09 00:20:48 +0200

    Add album image and thumbs assets

--- src/components/AlbumDetails.js
+++ src/components/AlbumDetails.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardItem from './CardItem';
 
-const AlbumDetails = props => {
+const AlbumDetails = ({ albumItem }) => {
+  const { title, artist, thumbnail_image, image } = albumItem;
+  const {
+    headerContentStyle,
+    thumbnailStyle,
+    thumbnailContainer,
+    headerTextStyle,
+    imageStyle } = styles;
   return (
     <Card>
       <CardItem>
-        <View>
+        <View style={thumbnailContainer}>
+          <Image source={{ uri: thumbnail_image }} style={thumbnailStyle} />
         </View>
-        <View style={styles.headerContentStyle}>
-          <Text>{props.albumItem.title}</Text>
-          <Text>{props.albumItem.artist}</Text>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
         </View>
+      </CardItem>
+      <CardItem>
+        <Image source={{ uri: image }} style={imageStyle} />
       </CardItem>
     </Card>
   );
@@ -22,7 +33,30 @@ const styles = {
   headerContentStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
+  },
+
+  headerTextStyle: {
+    fontSize: 18,
+  },
+
+  thumbnailStyle: {
+    width: 50,
+    height: 50,
+  },
+
+  thumbnailContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null,
   }
+
 };
 
 export default AlbumDetails;
